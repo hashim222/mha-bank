9f32163565fa5597d7a78ed1ac998bea7aeeba0d
hashim222 <aslamhashim19@gmail.com>
2022-05-08 10:49:39 +0000

    Created bank logo and welcome text

--- run.py
+++ run.py
@@ -1,18 +1,31 @@
 import gspread
 from google.oauth2.service_account import Credentials
+import pyfiglet
 
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
-    ]
+]
 
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('mha_bank')
 
-sales = SHEET.worksheet('Sheet1')
-data = sales.get_all_values()
+# sales = SHEET.worksheet('user_info')
+# data = sales.get_all_values()
 
+
+def get_user_details():
+    '''
+    Takes all the user info.
+    Inside the function runs multiple functions inside
+    '''
+    BANK_LOGO = pyfiglet.figlet_format('MHA  BANK')
+    print(BANK_LOGO)
+    print('Hello and welcome to mha bank.\n')
+
+
+get_user_details()

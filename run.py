ff758b2654fb62bbc2d6c58aa1ee3028fe618f09
hashim222 <aslamhashim19@gmail.com>
2022-05-10 13:13:17 +0000

    created usernname function and and updated google spreadsheet

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
+from time import sleep
 import gspread
 from google.oauth2.service_account import Credentials
 import pyfiglet
-from time import sleep
+
 
 
 SCOPE = [
@@ -13,10 +14,7 @@ SCOPE = [
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open('mha_bank')
-
-# sales = SHEET.worksheet('user_info')
-# data = sales.get_all_values()
+SHEET = GSPREAD_CLIENT.open('MHA_bank')
 
 
 def get_user_details():
@@ -26,13 +24,13 @@ def get_user_details():
     '''
     BANK_LOGO = pyfiglet.figlet_format('MHA  BANK')
     print(BANK_LOGO)
-    print('Hello and welcome to mha bank.\n')
+    print('Hello and welcome to MHA Bank🏦.\n')
 
     input('Press any BUTTON to start...')
     print('=========================================================================================')
     sleep(0.5)
+    enter_username()
     main()
-
 
 
 def validate_data(user_typed):
@@ -46,28 +44,55 @@ def validate_data(user_typed):
     except ValueError as err:
         print(f"Invalid data: {err} please try again. \n")
 
+
+def enter_username():
+    '''
+    Stores username
+    '''
+    while True:
+        print('Please keep the letter below 10 and above 5')
+        username = input('Create your username please: ')
+        SHEET.worksheet('user_info').update('A4', username)
+        if len(username) > 10:
+            print('The number of letters should not exceed 10. Please try again')
+            continue
+        elif len(username) < 5:
+            print('A minimum of five letters is required. Please try again')
+            continue
+        return username
+
+
 def deposit_money():
     '''
     Users inputs for deposit amount
     '''
-    user_deposit_money = float(input('how much would you like to deposit money? £'))
+    user_deposit_money = float(
+        input('how much would you like to deposit money? £'))
+    SHEET.worksheet('user_info').update_acell('B4', user_deposit_money)
     print(f'You deposit £{user_deposit_money} in your bank acc')
     return user_deposit_money
+
 
 def withdraw_money(amount):
     '''
     Users inputs for withdrawl amount.
     '''
-    user_withdraw_money = float(input('how much would you like to withdraw? £'))
+    user_withdraw_money = float(
+        input('how much would you like to withdraw? £'))
+    SHEET.worksheet('user_info').update_acell('C4', user_withdraw_money)
     updated_bal = amount - user_withdraw_money
     print(f'you have taken £{user_withdraw_money} from your bank acc')
     return updated_bal
+
+
 def view_balance(amount):
     '''
     Shows users to their total balance
     '''
     print(f'your updated balance is £{amount}')
+    SHEET.worksheet('user_info').update_acell('D4', amount)
     return amount
+
 
 def main():
     '''
@@ -76,28 +101,27 @@ def main():
     '''
     added_amount = deposit_money()
     removed_amount = withdraw_money(added_amount)
-    total_balance = view_balance(removed_amount)
-
-    while True:
-        sleep(1.5)
-        print('Please choose one of the following options: ')
-        print('\n1. Deposit Money\n2. Withdraw Money\n3. View Balance\n')
-        choose = input('Type here: ')
-        if choose == '1':
-            added_amount = deposit_money()
-            added_amount = added_amount + total_balance
-            print(f'your updated balance is £{added_amount}')
-        elif choose == '2':
-            removed_amount = withdraw_money(added_amount)
-            removed_amount = removed_amount - total_balance
-            print(removed_amount)
-        elif choose == '3':
-            view_balance(removed_amount)
-        elif choose == '':
-            print('please type a number')
-        else:
-            validate_data(choose)      
+    view_balance(removed_amount)
+    # total_balance =
+    # while True:
+    #     sleep(1.5)
+    #     print('Please choose one of the following options: ')
+    #     print('\n1. Deposit Money\n2. Withdraw Money\n3. View Balance\n')
+    #     choose = input('Type here: ')
+    #     if choose == '1':
+    #         added_amount = deposit_money()
+    #         total_balance = total_balance + added_amount
+    #         print(f'your updated balance is £{total_balance}')
+    #     elif choose == '2':
+    #         removed_amount = withdraw_money(added_amount)
+    #         total_balance = total_balance - removed_amount
+    #         print(total_balance)
+    #     elif choose == '3':
+    #         view_balance(removed_amount)
+    #     elif choose == '':
+    #         print('please type a number')
+    #     else:
+    #         validate_data(choose)
 
 
 get_user_details()
-
